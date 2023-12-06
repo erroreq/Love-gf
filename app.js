aaa82807d969792eefc89e79b7560f3e451b3a5d
erroreq <152640484+erroreq@users.noreply.github.com>
2023-12-06 23:09:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you tomorrow!";
+  question.innerHTML = "I love you so much 😘";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
